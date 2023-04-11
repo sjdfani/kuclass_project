8494f0c96b2185e03cf7c5df1e72f249c606a190
sajjad <faniam321@gmail.com>
2023-04-11 20:21:44 +0330

    fix bug

--- users/models.py
+++ users/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
-from .manager import CustomUserManager
+from .managers import CustomUserManager
 
 
 class User(AbstractUser):
