90748562a72f564ab777f5eb3c27a6054910cc45
sajjad <faniam321@gmail.com>
2023-05-17 21:57:56 +0330

    add email service config

--- kuclass_project/settings.py
+++ kuclass_project/settings.py
@@ -189,3 +189,11 @@ SWAGGER_SETTINGS = {
         }
     ]
 }
+
+# Email service config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_PASSWORD')
