84fbf1cfb779913600de5c8264b1a37a35934af3
sajjad <faniam321@gmail.com>
2023-07-13 00:35:12 +0330

    add new model

--- major/models.py
+++ major/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class GradeState(models.TextChoices):
+    BACHELOR = ('bachelor', 'Bachelor')
+    MASTER = ('master', 'Master')
+    PHD = ('phd', 'PhD')
+
+
+class Major(models.Model):
+    name = models.CharField(max_length=30, verbose_name=_('Name'))
+    status = models.BooleanField(_('Status'), default=False)
+    grade = models.CharField(
+        max_length=8, choices=GradeState.choices, verbose_name=_('Grade'))
+    created_at = models.DateField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
