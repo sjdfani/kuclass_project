2c31ae9fa85351f3b1f9ae4f057d807740bb8cfe
sajjad <faniam321@gmail.com>
2023-07-13 00:54:26 +0330

    complete major's name list

--- major/urls.py
+++ major/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import (
-    CreateMajor,
+    CreateMajor, MajorNameList,
 )
 
 app_name = 'major'
 
 urlpatterns = [
     path('create/', CreateMajor.as_view(), name='create'),
+    path('list/<grade:str>/', MajorNameList.as_view(), name='create'),
 ]
--- major/views.py
+++ major/views.py
@@ -1,8 +1,8 @@
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import IsAdminUser
+from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .models import Major
 from .serializers import (
-    MajorListSerializer
+    MajorListSerializer, MajorNameListSerializer
 )
 
 
@@ -10,3 +10,12 @@ class CreateMajor(CreateAPIView):
     permission_classes = (IsAdminUser,)
     serializer_class = MajorListSerializer
     queryset = Major.objects.all()
+
+
+class MajorNameList(ListAPIView):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = MajorNameListSerializer
+
+    def get_queryset(self):
+        grade = self.kwargs.get('grade')
+        return Major.objects.filter(grade=grade, status=True)
