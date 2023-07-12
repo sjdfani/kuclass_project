0308a59d1bcdd0ada204c95a6279d1ca78e093e3
sajjad <faniam321@gmail.com>
2023-07-13 01:05:14 +0330

    complete update destroy major

--- major/urls.py
+++ major/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from .views import (
-    CreateMajor, MajorNameList,
+    CreateMajor, MajorNameList, MajorRetrieveUpdateDestroy,
 )
 
 app_name = 'major'
 
 urlpatterns = [
     path('create/', CreateMajor.as_view(), name='create'),
-    path('list/<grade:str>/', MajorNameList.as_view(), name='list'),
+    path('list/<str:grade/', MajorNameList.as_view(), name='list'),
+    path('list/<int:pk>/', MajorRetrieveUpdateDestroy.as_view(),
+         name='retrieve_update_destroy_major'),
 ]
--- major/views.py
+++ major/views.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from .models import Major
 from .serializers import (
@@ -19,3 +19,9 @@ class MajorNameList(ListAPIView):
     def get_queryset(self):
         grade = self.kwargs.get('grade')
         return Major.objects.filter(grade=grade, status=True)
+
+
+class MajorRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAdminUser,)
+    serializer_class = MajorListSerializer
+    queryset = Major.objects.all()
