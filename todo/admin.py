14184d39779c881c6da744e98e73de52e89dc982
sajjad <faniam321@gmail.com>
2023-07-12 23:54:25 +0330

    complete admin

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Job
 
-# Register your models here.
+
+class JobAdmin(admin.ModelAdmin):
+    list_display = ('user', 'end_at', 'complete_date', 'state')
+    list_filter = ('state',)
+    list_editable = ('state',)
+
+
+admin.site.register(Job, JobAdmin)
