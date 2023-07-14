1456f70fff7e9990b3571af43e8a847f775747e1
sajjad <faniam321@gmail.com>
2023-07-14 16:36:10 +0330

    fix bug

--- major/urls.py
+++ major/urls.py
@@ -7,7 +7,7 @@ app_name = 'major'
 
 urlpatterns = [
     path('create/', CreateMajor.as_view(), name='create'),
-    path('list/<str:grade/', MajorNameList.as_view(), name='list'),
+    path('list/<str:grade>/', MajorNameList.as_view(), name='list'),
     path('list/<int:pk>/', MajorRetrieveUpdateDestroy.as_view(),
          name='retrieve_update_destroy_major'),
 ]
