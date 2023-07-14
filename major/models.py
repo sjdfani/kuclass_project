1ebc44fba1900f98419d88afee357dedbd57b8bd
sajjad <faniam321@gmail.com>
2023-07-14 17:14:31 +0330

    change status default

--- major/models.py
+++ major/models.py
@@ -10,7 +10,7 @@ class GradeState(models.TextChoices):
 
 class Major(models.Model):
     name = models.CharField(max_length=30, verbose_name=_('Name'))
-    status = models.BooleanField(_('Status'), default=False)
+    status = models.BooleanField(_('Status'), default=True)
     grade = models.CharField(
         max_length=8, choices=GradeState.choices, verbose_name=_('Grade'))
     created_at = models.DateField(auto_now_add=True)
